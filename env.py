213c030fb8a737e5304d9e2b9aa0963e0af4c30a
Ubuntu <ubuntu@ip-172-31-42-46.ec2.internal>
2020-02-24 13:01:13 +0000

    Initial commit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['HOST'] =  '83ce574be4f746b4868c70b28edcf769.vfs.cloud9.us-east-1.amazonaws.com'
+os.environ['SECRET_KEY'] = '9ggy3!u+)t&b%d9@iio^q3tgugkfk7y8lvo=%^*m#-4^4y!uzz'
+os.environ['EMAIL_ADDRESS'] = 'devtestingacc95@gmail.com'
+os.environ['EMAIL_PASSWORD'] = 'testingtesting12'
